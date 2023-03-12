7d007c417d89dc38e20040a45796242d027716e2
tertiux44 <secondsonofadebayo@gmail.com>
2023-03-12 19:57:10 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,79 @@
+const body = document.querySelector("body")
+
+window.addEventListener("scroll", ()=>{
+    if(window.scrollY > 80){
+        body.classList.add("scrolled")
+    } else{
+        body.classList.remove("scrolled")
+    }
+})
+
+const hamburger = document.querySelector(".hamburger")
+const backdrop = document.querySelector(".backdrop")
+
+hamburger.addEventListener("click", ()=>{
+    body.classList.toggle("show-menu")
+})
+
+backdrop.addEventListener("click", ()=>{
+    body.classList.remove("show-menu")
+})
+
+window.scrollTo({
+    top: 0,
+    left:0,
+    behavior: "smooth"
+})
+
+function reload(){
+    location.reload()
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
+
+function closeMenu(){
+    body.classList.remove("show-menu")
+}
+
+window.addEventListener("load", ()=>{
+    setTimeout(function(){
+        body.classList.remove("loading")
+    }, 1000)
+})
+
+setTimeout(
+    function(){
+        body.classList.remove("loading")
+    }, 8000
+)
+
+
+
+
+
+
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            // entry.target.classList.remove('show')
+        }
+    });
+},
+{
+    threshold: 0.2,
+    // rootMargin: "-100px"
+}
+
+
+);
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
